b6db8ca3d2ec726408cd4c1c43abfe5cd589b169
Harsh <tiwariharsh421@gmail.com>
2024-08-03 21:34:36 +0530

    Remove Duplicates Leetcode Easy Problem

---
+++ Leetcode Problems/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int ans = removeDuplicates(arr);
+        System.out.println(ans);
+    }
+
+    static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                swap(nums, j, i);
+            }
+        }
+
+        return i + 1;
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
