4d50f1ac861dbceeba19f819da8259ae712180e0
Harsh <tiwariharsh421@gmail.com>
2024-08-13 22:49:59 +0530

    Set Matrix Zeroes : Leetcode medium

---
+++ Leetcode Problems/SetZeroes.java
@@ -0,0 +1,39 @@
+// Set Matrix Zeroes: Leetcode Medium
+
+public class SetZeroes {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+        setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] == 1 || cols[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
