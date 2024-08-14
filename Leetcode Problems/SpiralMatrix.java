2ff7cee4be6ca9f23ec32c471cf01efe83afaf58
Harsh <tiwariharsh421@gmail.com>
2024-08-14 21:03:41 +0530

    Spiral Matrix: Leetcode medium

---
+++ Leetcode Problems/SpiralMatrix.java
@@ -0,0 +1,59 @@
+// Spiral Matrix: Leetcode medium
+
+import java.util.*;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+
+        List<Integer> ans = spiralOrder(matrix);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    static List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+
+        if (matrix == null || matrix.length == 0)
+            return ans;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+            }
+            left++;
+        }
+
+        return ans;
+    }
+}
